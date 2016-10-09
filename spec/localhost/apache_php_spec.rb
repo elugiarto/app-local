c7365e7fec27eea0aa3807591330074d4659c22f
Daniel Tedman <dbtedman@gmail.com>
2016-10-09 22:00:46 +1000

    Extract get ignore ssl method to helper.

--- spec/localhost/apache_php_spec.rb
+++ spec/localhost/apache_php_spec.rb
@@ -2,9 +2,6 @@
 # Defines the Apache PHP specification.
 #
 
-require 'nokogiri'
-require 'open-uri'
-require 'net/https'
 require_relative '../spec_helper'
 
 describe port(443) do
@@ -17,9 +14,8 @@ describe service('httpd') do
 end
 
 describe 'PHP about page "/about.php"' do
+  about_document = get_doc_ignore_ssl("#{SITE}/about.php")
+  expected_version = /^PHP Version 5\.5\.\d+$/
 
-  url = 'https://localhost:8443/about.php'
-  doc = Nokogiri::HTML(open(url, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
-
-  it { !!(/^PHP Version 5\.5\.\d+$/ =~ doc.xpath('//h1').first.content) }
+  it { !!(expected_version =~ about_document.xpath('//h1').first.content) }
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
-
 require 'serverspec'
+require 'nokogiri'
+require 'open-uri'
+require 'net/https'
+
+SITE = 'https://localhost:8443'
+
+def get_doc_ignore_ssl url
+  Nokogiri::HTML(open(url, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
+end
