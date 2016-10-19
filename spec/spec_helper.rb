14f25f22b187985898da698121f672d2a4e64a61
Daniel Tedman <dbtedman@gmail.com>
2016-10-19 19:01:37 +1000

    Add todo comments.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require 'nokogiri'
 require 'open-uri'
 require 'net/https'
 
+# TODO: Is this not 443 internally? If not, this will need to be based on site config.
 SITE = 'https://localhost:8443'
 
 def get_doc_ignore_ssl url
